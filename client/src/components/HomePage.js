d3cda6d1574e8dc1939d16197821b0c1ae2058ad
Qihao Wang <qw2@illinois.edu>
2019-10-28 14:45:09 -0500

    post axios

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import PostForm from './PostForm';
 import Post from './Post';
 import './HomePage.css';
+import axios from 'axios';
+
+const home_url = "http://localhost:3000/"
+
 class HomePage extends Component {
   constructor(props) {
       super(props);
@@ -16,6 +20,21 @@ class HomePage extends Component {
       newPosts.unshift(newPost);
       this.setState({posts: newPosts});
       console.log(this.state.posts);
+      //set format for the sending data
+      const data_send = {
+        username: newPost.username,
+        time: newPost.time,
+        content: newPost.content,
+        comments: newPost.comments
+      };
+      console.log('data send', data_send);
+      // console.log('finish debug');
+      axios.post(home_url + "api/this",  data_send)
+        .then(res =>{
+          console.log("test");
+          console.log(res);
+          console.log('response data', res.data);
+        })
   }
 
   render() {
