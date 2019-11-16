8e300d47bc5e57962c683814ddc03066069808c3
Isabell L <il3@illinois.edu>
2019-11-16 15:54:29 -0600

    fixed login page binding error

--- client/src/components/RouterLayout.js
+++ client/src/components/RouterLayout.js
@@ -6,10 +6,24 @@ import { Menu, Layout} from 'antd';
 import 'antd/dist/antd.css';
 const {Header, Content} = Layout;
 class RouterLayout extends Component {
+  /*
+  constructor(props){
+		super(props);
+		this.state = {
+      username: this.props.location.state.username
+      password: this.props.location.state.password
+		}
+	}
+  */
   render() {
     const renderedPage = this.props.location.pathname === '/Home' ? <HomePage/> : <SearchPage/>;
     console.log(this.props.location.pathname);
     return (
+    /*
+    if (this.state.username == null) {
+      <Redirect to={{pathname: '/'}}/>
+    }
+    */
     <div>
       <Layout>
         <Header>
@@ -27,6 +41,10 @@ class RouterLayout extends Component {
               <Menu.Item key='/Search'>
                 Search
                 <Link to='/Search'/>
+              </Menu.Item>
+              <Menu.Item key='/'>
+                Sign out
+                <Link to='/'/>
               </Menu.Item>
           </Menu>
         </Header>
