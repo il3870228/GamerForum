ae6454bd5af44cfab28994fe069e52cf1c4fe38e
jiayi <jiayiwu3@illinois.edu>
2019-12-04 15:15:09 -0600

    merge

--- expressjs/py/neo4jAddFriend.py
+++ expressjs/py/neo4jAddFriend.py
@@ -7,7 +7,7 @@ db = GraphDatabase.driver(uri, auth=("neo4j", "12345678"))
 userid = sys.argv[1]
 friendid = sys.argv[2]
 
-cqlNodeQuery = " MATCH (x: USER {userid: " + userid + "}), (y: USER {userid: " + friendid + "}) CREATE (x)-[r:Friend]->(y)"
+cqlNodeQuery = " MATCH (x: USER {userid: " + userid + "}), (y: USER {userid: " + friendid + "}) MERGE (x)-[r:Friend]->(y)"
 
 with db.session() as graphDB_Session:
     nodes = graphDB_Session.run(cqlNodeQuery)
